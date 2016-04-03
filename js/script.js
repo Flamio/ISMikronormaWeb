c1f9dde1ea43cd784f53b80ff507a6f43b662d4d
mmenshikov <maksim-menshikov@mail.ru>
2016-04-03 15:23:33 +0400

    сделал перемотку

--- js/script.js
+++ js/script.js
@@ -20,12 +20,12 @@ function onPlayPause()
 
 function onForward()
 {
-	document.getElementById("videoField").currentTime = document.getElementById("videoField").duration;
+	document.getElementById("videoField").currentTime+=2
 }
 
 function onBackward()
 {
-	document.getElementById("videoField").currentTime = 0;
+	document.getElementById("videoField").currentTime-=2;
 }
 
 function onSpeed(speed)
@@ -59,3 +59,79 @@ function onProcessClick(id)
             processSelected = id;
         }
 }
+
+// javascript-код голосования из примера
+/*02
+function vote() {
+03
+    // (1) создать объект для запроса к серверу
+04
+    var req = getXmlHttp() 
+05
+        
+06
+        // (2)
+07
+    // span рядом с кнопкой
+08
+    // в нем будем отображать ход выполнения
+09
+    var statusElem = document.getElementById('vote_status')
+10
+     
+11
+    req.onreadystatechange = function() { 
+12
+        // onreadystatechange активируется при получении ответа сервера
+13
+ 
+14
+        if (req.readyState == 4) {
+15
+            // если запрос закончил выполняться
+16
+ 
+17
+            statusElem.innerHTML = req.statusText // показать статус (Not Found, ОК..)
+18
+ 
+19
+            if(req.status == 200) {
+20
+                 // если статус 200 (ОК) - выдать ответ пользователю
+21
+                alert("Ответ сервера: "+req.responseText);
+22
+            }
+23
+            // тут можно добавить else с обработкой ошибок запроса
+24
+        }
+25
+ 
+26
+    }
+27
+ 
+28
+       // (3) задать адрес подключения
+29
+    req.open('GET', '/ajax_intro/vote.php', true); 
+30
+ 
+31
+    // объект запроса подготовлен: указан адрес и создана функция onreadystatechange
+32
+    // для обработки ответа сервера
+33
+      
+34
+        // (4)
+35
+    req.send(null);  // отослать запрос
+36
+   
+37
+        // (5)
+38
+    statusElem.innerHTML = 'Ожидаю ответа сервера...'*/
